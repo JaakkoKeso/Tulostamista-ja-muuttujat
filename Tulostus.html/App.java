ed8a0bc3321228bc6cf3d5e9599b74d44ff82687
JaakkoKeso <amk1006462@student.hamk.fi>
2024-10-11 14:53:15 +0300

    Create App.java

---
+++ Tulostus.html/App.java
@@ -0,0 +1,40 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Tähän kirjoitellaan koodia
+
+        String nimi = "Jaakko Keso";
+
+        System.out.println("nimeni on " + nimi + " ja olen valkeakoskelta");
+
+        int luku1 = 5;
+        int luku2 = 10;
+        int tulos;
+
+        luku1 = 2;
+        tulos = luku1+luku2;
+        System.out.println(luku1);
+        System.out.println("Se tulos on tossa " + tulos);
+        
+
+
+
+        System.out.println("Eka ohjelma");
+
+        System.out.println("H");
+        System.out.println("A");
+        System.out.println("M ");
+        System.out.println("K ");
+        System.out.println("I ");
+
+
+
+
+
+        
+
+
+
+
+    }
+}
